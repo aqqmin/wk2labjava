acaf15964e389fb253772126be6a1bd6622b4724
aqqmin <69249534+aqqmin@users.noreply.github.com>
2022-10-03 11:03:43 -0700

    Create Wk2Lab.java

---
+++ Wk2Lab.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Wk2Lab {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        CharCount(scan);
+        IsPrime(scan);
+        NthPrime(scan);
+        StringRev(scan);
+        IntOper(scan);
+        IntQuizzer(scan);
+    }
+
+    static void CharCount(Scanner scan) {
+        scan.nextLine();
+    }
+    
+    static void IsPrime(Scanner scan) {
+        scan.nextLine();
+    }
+
+    static void NthPrime (Scanner scan) {
+        scan.nextLine();
+    }
+
+    static void StringRev(Scanner scan) {
+        scan.nextLine();
+    }
+
+    static void IntOper(Scanner scan) {
+        scan.nextLine();
+    }
+
+    static void IntQuizzer(Scanner scan) {
+        
+    }
+
+}
+
